0c7e5369d057ae69d45d1dc99981ba47a4f4bf9d
Sabarinathan R S <nsabari765@gmail.com>
2026-01-05 23:20:53 +0530

    05-01-2026 Night Practice

--- Source/FindNthPrimeNumber.cs
+++ Source/FindNthPrimeNumber.cs
@@ -1,36 +1,64 @@
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
-
-using System;
+using System.Diagnostics;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine ("Try programiz.pro");
-    }
-    
-    public int GetNPrimeNumber(int n)
-    {
-        int primeRange = 0;
-        var count = 0;
-        var temp = 2;
-        
-        while(temp <= n)
+        var watch = Stopwatch.StartNew();
+
+        string? userDecision = string.Empty;
+
+        do
         {
-            for(int i = 1; i <= temp; i++)
+            Console.Write("Enter Range:");
+            int range = Convert.ToInt32(Console.ReadLine());
+
+            int nthPrime = GetNthPrimeNumber(range);
+            Console.WriteLine($"Your Prime = {nthPrime}");
+
+            Console.Write("Do you want to continue (Y/N): ");
+            userDecision = Console.ReadLine();
+
+        } while (userDecision.ToLower() == "yes");
+
+
+        watch.Stop();
+
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds / 60} s");
+    }
+
+    private static int GetNthPrimeNumber(int range)
+    {
+        int number = 2;
+        int primeNumber = 0;
+        int count = 1;
+
+        while(count <= range)
+        {
+            if(IsPrime(number))
             {
-                if((temp % i) == 0)
-                {
-                    count++;
-                }
+                primeNumber = number;
+                count++;
             }
-            
-            if(count == 2)
+
+            number++;
+        }
+
+        return primeNumber;
+    }
+
+    private static bool IsPrime(int number)
+    {
+        int count = 0;
+
+        for(int i = 1; i <= number; i++)
+        {
+            if(number % i == 0)
             {
-                primeRange++;
-                temp = 
+                count++;
             }
         }
+
+        return count == 2;
     }
 }
